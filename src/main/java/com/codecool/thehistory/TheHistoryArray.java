fa66d3736d660f983c82d6f4c524160e7b309d33
Clethcriss <balazs.borsi.1986@gmail.com>
2018-09-02 21:07:01 +0200

    array and arraylist

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -1,6 +1,7 @@
 package com.codecool.thehistory;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class TheHistoryArray implements TheHistory {
 
@@ -12,32 +13,124 @@ public class TheHistoryArray implements TheHistory {
     @Override
     public void add(String text) {
         //TODO: check the TheHistory interface for more information
+        String[] newArray = text.split("\\s+");
+        String[] tempArray = new String[this.wordsArray.length];
+        System.arraycopy(this.wordsArray, 0, tempArray, 0, this.wordsArray.length);
+        this.wordsArray = new String[newArray.length + this.wordsArray.length];
+        System.arraycopy(tempArray, 0, this.wordsArray, 0, tempArray.length);
+        System.arraycopy(newArray, 0, this.wordsArray, tempArray.length, newArray.length );
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         //TODO: check the TheHistory interface for more information
+        int counter = 0;
+        for (int i = 0; i < this.wordsArray.length; i++){
+            if (Objects.equals(this.wordsArray[i], wordToBeRemoved)){
+                counter++;
+            }
+        }
+        String[] tempArray = new String[this.wordsArray.length - counter];
+        counter = 0;
+        for (int i = 0; i < this.wordsArray.length; i++){
+            if (!Objects.equals(this.wordsArray[i], wordToBeRemoved)){
+                tempArray[counter] = this.wordsArray[i];
+                counter++;
+            }
+        }
+        this.wordsArray = new String[tempArray.length];
+        System.arraycopy(tempArray, 0, this.wordsArray, 0, tempArray.length);
     }
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        return this.wordsArray.length;
     }
 
     @Override
     public void clear() {
         //TODO: check the TheHistory interface for more information
+        this.wordsArray = new String[0];
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
         //TODO: check the TheHistory interface for more information
+        for (int i = 0; i < this.wordsArray.length; i++) {
+            if (Objects.equals(this.wordsArray[i], from)) {
+                this.wordsArray[i] = to;
+            }
+        }
     }
 
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
         //TODO: check the TheHistory interface for more information
+        if (fromWords.length == toWords.length) {
+            for (int i = 0; i < this.wordsArray.length; i++) {
+                if (fromWords[0].equals(this.wordsArray[i])) {
+                    boolean check = true;
+                    for (int j = 1; j < fromWords.length; j++) {
+                        if (!fromWords[j].equals(this.wordsArray[i + j])) {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check) {
+                        for (int j = 0; j < toWords.length; j++) {
+                            this.wordsArray[i + j] = toWords[j];
+                        }
+                        i += fromWords.length - 1;
+                    }
+                }
+            }
+        } else {
+            int counter = 0;
+            for (int i = 0; i < this.wordsArray.length; i++) {
+                if (fromWords[0].equals(this.wordsArray[i])) {
+                    boolean check = true;
+                    for (int j = 1; j < fromWords.length; j++) {
+                        if ((i + j) >= this.wordsArray.length || !fromWords[j].equals(this.wordsArray[i + j])) {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check) {
+                        counter++;
+                        i += fromWords.length - 1;
+                    }
+                }
+            }
+            String[] temp = new String[this.wordsArray.length - (fromWords.length * counter) + (toWords.length * counter)];
+            int tempIndex = 0;
+            for (int i=0; i< this.wordsArray.length;i++){
+                if(fromWords[0].equals(this.wordsArray[i])){
+                    boolean check = true;
+                    for (int j = 1; j < fromWords.length; j++){
+                        if ((i+j) >= this.wordsArray.length || !fromWords[j].equals(this.wordsArray[i + j])){
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check) {
+                        for (int j = 0; j< toWords.length;j++){
+                            temp[tempIndex] = toWords[j];
+                            tempIndex++;
+                        }
+                        i += fromWords.length-1;
+                    } else {
+                        temp[tempIndex] = wordsArray[i];
+                        tempIndex++;
+                    }
+                } else {
+                    temp[tempIndex] = wordsArray[i];
+                    tempIndex++;
+                }
+            }
+            this.wordsArray= temp;
+
+        }
     }
 
     @Override
--- src/main/java/com/codecool/thehistory/TheHistoryArrayList.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArrayList.java
@@ -1,9 +1,6 @@
 package com.codecool.thehistory;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class TheHistoryArrayList implements TheHistory {
     /**
@@ -14,32 +11,82 @@ public class TheHistoryArrayList implements TheHistory {
     @Override
     public void add(String text) {
         //TODO: check the TheHistory interface for more information
+        String[] newArray = text.split("\\s+");
+        this.wordsArrayList.addAll(Arrays.asList(newArray));
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         //TODO: check the TheHistory interface for more information
+        ListIterator<String> listIterator = this.wordsArrayList.listIterator();
+        while (listIterator.hasNext()) {
+            if (listIterator.next().equals(wordToBeRemoved))
+                listIterator.remove();
+        }
     }
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        return this.wordsArrayList.size();
     }
 
     @Override
     public void clear() {
         //TODO: check the TheHistory interface for more information
+        this.wordsArrayList = new ArrayList<>();
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
         //TODO: check the TheHistory interface for more information
+        ListIterator<String> listIterator = this.wordsArrayList.listIterator();
+        while (listIterator.hasNext()) {
+            if (listIterator.next().equals(from))
+                listIterator.set(to);
+        }
     }
 
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
         //TODO: check the TheHistory interface for more information
+        if (fromWords.length == toWords.length) {
+            for (int i = 0; i < this.wordsArrayList.size(); i++) {
+                if (fromWords[0].equals(this.wordsArrayList.get(i))) {
+                    boolean check = true;
+                    for (int j = 1; j < fromWords.length; j++) {
+                        if ((j + i) >= this.wordsArrayList.size() || !fromWords[j].equals(this.wordsArrayList.get(i + j))) {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check) {
+                        for (int j = 0; j < fromWords.length; j++) {
+                            this.wordsArrayList.set((i + j), toWords[j]);
+                        }
+                    }
+                }
+            }
+        } else {
+            for (int i = 0; i < this.wordsArrayList.size(); i++) {
+                if (fromWords[0].equals(this.wordsArrayList.get(i))) {
+                    boolean check = true;
+                    for (int j = 1; j < fromWords.length; j++) {
+                        if ((j + i) >= this.wordsArrayList.size() || !fromWords[j].equals(this.wordsArrayList.get(i + j))) {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (check) {
+                        for (int j = 0; j < fromWords.length; j++) {
+                            this.wordsArrayList.remove(i);
+                        }
+                        this.wordsArrayList.addAll(i, Arrays.asList(toWords));
+                        i += toWords.length - 1;
+                    }
+                }
+            }
+        }
     }
 
     @Override
